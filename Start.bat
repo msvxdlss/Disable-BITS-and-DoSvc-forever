2b0791f2e53023b500fb9d3c2448a54f02702388
Димка <ya.lpy@ya.ru>
2024-05-25 08:42:46 +0300

    Update Start.bat
    Добавлен 30-секундный дилей

--- Start.bat
+++ Start.bat
@@ -1,6 +1,7 @@
 @echo off
 set NEW_BAT_PATH=C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\DoSvc_BITS.bat
 ( echo @echo off
+echo timeout /t 30 /nobreak >nul
 echo net stop BITS
 echo sc config BITS start=Disabled
 echo net stop DoSvc
